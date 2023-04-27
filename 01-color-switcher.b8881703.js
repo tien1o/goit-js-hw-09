e60b432beaf0dfdf6e7c4d23b16df8480586a696
tien1o <118685409+tien1o@users.noreply.github.com>
2023-04-27 12:57:31 +0000

    Deploying to gh-pages from @ tien1o/goit-js-hw-09@c91637f2334e49617922a60da5d1d4451da86979 🚀

--- 01-color-switcher.b8881703.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let a=null;d.disabled=!0;e.addEventListener("click",(()=>{e.disabled=!0,d.disabled=!1,a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),d.addEventListener("click",(()=>{clearInterval(a),d.disabled=!1,e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.b8881703.js.map
